59aa1b1b2fc2257544359a5082c9587b96119d26
Greg Brail <greg@apigee.com>
2013-04-08 17:37:00 -0700

    Start 0.5.6.

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-mvn release:update-versions -DautoVersionSubmodules=true 
+if [ $# -ne 1 ]
+then
+  echo "Usage: update-versions <version>"
+  exit 1
+fi
+
+mvn release:update-versions -DautoVersionSubmodules=true \
+  -DdevelopmentVersion=${1}

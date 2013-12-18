4d59e4b61d37048553888eaab5d06329f56c551d
Greg Brail <greg@apigee.com>
2013-12-17 17:48:17 -0800

    Try to fix JaCoCo coverage for this module.

---
+++ nodetests/src/main/java/io/apigee/trireme/nodetests/Dummy.java
@@ -0,0 +1,9 @@
+package io.apigee.trireme.nodetests;
+
+/**
+ * The sole purpose of this class is to prevent JaCoCo from barfing on this module, since the most interesting
+ * code coverage data will come from this module, and JaCoCo doesn't work unless target/classes has something in it.
+ */
+public class Dummy
+{
+}
